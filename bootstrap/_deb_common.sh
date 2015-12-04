a96fbdfb69d90394c566690ba1c1d3ca21f3752e
Stephen Oberholtzer <stevie@qrpff.net>
2015-12-03 22:16:30 -0500

    Allow NOROOT to avoid becoming root or doing things that require it

--- bootstrap/_deb_common.sh
+++ bootstrap/_deb_common.sh
@@ -18,7 +18,11 @@
 #
 # - Debian 6.0.10 "squeeze" (x64)
 
-apt-get update
+function root_ok {
+	return [ "$NOROOT" -ne "1" ]
+}
+
+root_ok && apt-get update
 
 # virtualenv binary can be found in different packages depending on
 # distro version (#346)
@@ -32,7 +36,7 @@ if apt-cache show python-virtualenv > /dev/null ; then
   virtualenv="$virtualenv python-virtualenv"
 fi
 
-apt-get install -y --no-install-recommends \
+root_ok && apt-get install -y --no-install-recommends \
   git \
   python \
   python-dev \
@@ -45,6 +49,6 @@ apt-get install -y --no-install-recommends \
   ca-certificates \
 
 if ! command -v virtualenv > /dev/null ; then
-  echo Failed to install a working \"virtualenv\" command, exiting
+  echo Failed to find or install a working \"virtualenv\" command, exiting
   exit 1
 fi
